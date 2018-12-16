d119e3eede15edc38915baa737dddae89b0c780f
zl190 <zl190@duke.edu>
2018-12-16 03:42:13 -0500

    fix some too long line to meet pep8 requirement

--- client.py
+++ client.py
@@ -3,7 +3,8 @@ import toolbox_jason as jtb
 
 
 def test_api_toolbox_validate_email():
-    r_status = requests.post("http://127.0.0.1:5000/api/toolbox/validate_email", json={'email': '111@duke.edu'})
+    r_status = requests.post("http://127.0.0.1:5000/api/toolbox/validate_email",
+                             json={'email': '111@duke.edu'})
     # status = r_status.json()
     print(r_status)
 
@@ -13,20 +14,25 @@ def test_api_upload():
     encoded_buf = jtb.encode_base64(buffer.read())
 
     print(type(encoded_buf))
-    # r_status = requests.post("http://vcm-7306.vm.duke.edu:5000/api/toolbox/validate_email",
+    # r_status = requests.post("http://vcm-7306.vm.duke.edu:5000/api/
+    # toolbox/validate_email",
     # json={'email':'111@duke.edu'})
-    # r_status = requests.post("http://vcm-7306.vm.duke.edu:5000/api/upload", json={'email':'111@duke.edu', '
+    # r_status = requests.post("http://vcm-7306.vm.duke.edu:5000/api/upload",
+    # json={'email':'111@duke.edu', '
     # imageset':str(encoded_buf)})
 
     r_status = requests.post("http://127.0.0.1:5000/api/upload",
-                             json={'email': '111@duke.edu', 'imageset': encoded_buf.decode('utf8')})
+                             json={'email': '111@duke.edu',
+                                   'imageset': encoded_buf.decode('utf8')})
 
     status = r_status.json()
     print(status)
 
 
 def test_api_download():
-    r_status = requests.post("http://127.0.0.1:5000/api/download_zip", json={'email':'111@duke.edu', 'imageset':'image_data'})
+    r_status = requests.post("http://127.0.0.1:5000/api/download_zip",
+                             json={'email':'111@duke.edu',
+                                   'imageset':'image_data'})
     print(r_status.content)
     jtb.unzip_buffer(r_status.content, 'tmp/unzip')
 
