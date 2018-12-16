7bf10c1b550ac78cad424cde923f297046738ccd
zl190 <zl190@duke.edu>
2018-12-15 20:51:52 -0500

    add more detailed docstring for functions in database_function_call; unify the numpy docstring style

--- database_func_call.py
+++ database_func_call.py
@@ -1,8 +1,54 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author: Jason L
+"""wrapper of mongoDB database operations
 
-import sys
+This module provides an database module and basic database operations of it
+
+
+Module Attributes
+----------
+user_email: EmailField(key)
+    expect a unique and valid email_format string
+
+image_data: CharField
+    expect a path_string where the original data stored
+
+brew_image_data: CharField
+    expect a path_string where the brewed data stored
+
+actions: ListField
+    expect as ['upload', 'action1', 'action2', ...]
+
+timestamps: ListField
+    expect as [timestamp0, timestamp1, timestamp2, ...]
+
+
+Database Operations
+-----------
+operation on record
+    save_new_record(r): create a new record
+
+    # Todo:
+    query a record
+    delete a record
+
+operation on field
+    query_a_record(key, field): get the value of a field
+
+    update_a_record(key, field): update the value of a field
+
+    clear_a_field(key, field): clear the value of a field
+
+    clear_fields(key, field_list): clear the value of multiply field
+
+
+.. _PyMODM https://pymodm.readthedocs.io/en/latest/
+.. _MongoDB https://docs.mongodb.com
+
+"""
+
+
 from pymodm import connect, MongoModel, fields
 from datetime import datetime
 
@@ -11,10 +57,8 @@ connect("mongodb://void001:goduke18@ds129484.mlab.com:29484/bme590final")
 
 
 # database module
-
-
 class Imageset(MongoModel):
-    """Data Class used by MongoDB
+    """Data Module used by MongoDB
 
     """
     user_email = fields.EmailField(primary_key=True)
@@ -25,17 +69,19 @@ class Imageset(MongoModel):
 
 
 def save_new_record(r):
-    """save a new record
-    user_email  and image_data must be given
-    actions and timestamps field will be set automatically
+    """create a new record
+
+    Fields `user_email` and `image_data` must be given, while
+    `actions` and `timestamps` field will be set automatically
 
     Parameters
     ----------
-    r: dictionary contains 'user_email' and 'image_data' field
+    r : dict
+        must contains `user_email` and `image_data` field
 
     Returns
     -------
-    True
+
     """
     q = Imageset(
         r['user_email'],
@@ -44,7 +90,6 @@ def save_new_record(r):
         actions=['upload'],
         timestamps=[datetime.now()])
     q.save()
-    return True
 
 
 def query_a_record(key, field):
@@ -52,12 +97,13 @@ def query_a_record(key, field):
 
     Parameters
     ----------
-    key: string
-    field: string
+    key : string
+    field : string
 
     Returns
     -------
-    query value
+    string or list
+        depends on the query field
     """
     q = Imageset.objects.raw({"_id": key}).first()
     assert (q is not None)
@@ -67,11 +113,14 @@ def query_a_record(key, field):
 def update_a_record(key, field, value):
     """set a new value for a field
 
+    update a new value for CharField
+    append the new value for ListField
+
     Parameters
     ----------
-    key: string
-    field: string
-    value: according to the filed
+    key : string
+    field : string
+    value : string
 
     Returns
     -------
@@ -89,12 +138,14 @@ def update_a_record(key, field, value):
 
 
 def clear_a_field(key, field):
-    """set value of a field to None
+    """clear the value of a field
+
+    set value of a field to None
 
     Parameters
     ----------
-    key
-    field
+    key : string
+    field : string
 
     Returns
     -------
@@ -107,13 +158,15 @@ def clear_a_field(key, field):
 
 
 def clear_fields(key, field_list):
-    """set values of multiple fields to None
+    """clear the value of multiply field
+
+    set values of multiple fields to None,
     not exist fields given by field_list will be ignored
 
     Parameters
     ----------
-    key
-    field_list
+    key : string
+    field_list : list
 
     Returns
     -------
